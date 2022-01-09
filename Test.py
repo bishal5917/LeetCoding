cd86af680d506214c79e861a1456ff1d82535a32
Bishal Poudel <poudelb172@gmail.com>
2022-01-09 21:51:20 +0545

    -1,-1 of 2d array

--- Test.py
+++ Test.py
@@ -6,9 +6,14 @@
 # array.append([1]+[2])
 # print(array)
 
-even = [1, 7]
-odd = [6, 8]
-maxFind1 = max(even, odd)  # this doesnot do subtraction(watches the last)
-maxFind2 = max(even, odd, key=lambda x: x[1]-x[0])  # this do by subtracting
-# Note : key is keyword argument for max()
-print(maxFind1, maxFind2)
+# even = [1, 7]
+# odd = [6, 8]
+# maxFind1 = max(even, odd)  # this doesnot do subtraction(watches the last)
+# maxFind2 = max(even, odd, key=lambda x: x[1]-x[0])  # this do by subtracting
+# # Note : key is keyword argument for max()
+# print(maxFind1, maxFind2)
+
+# array=[[3,4,1,9],
+#          [1,3,4,6]]
+# print(array[-1][-1])
+
