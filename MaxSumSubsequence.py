2968e0986c831a469b18eae967a16b91329d6cb6
Bishal Poudel <poudelb172@gmail.com>
2022-04-30 21:47:55 +0545

    Max Sum Subsequence prob

--- MaxSumSubsequence.py
+++ MaxSumSubsequence.py
@@ -1,16 +1,14 @@
 def MaxSumSubsequence(arr):
-    sums = [0 for i in range(len(arr))]
-    sequence = [None for i in range(len(arr))]
+    sums = [nums for nums in arr]
+    sequence = [None for x in arr]
     sums[0] = arr[0]
 
     for i in range(1, len(arr)):
         for j in range(0, i):
-            if arr[j] < arr[i]:
+            if arr[j] < arr[i] and sums[j] + arr[i] >= sums[i]:
                 sums[i] = max(sums[i], sums[j]+arr[i])
                 sequence[i] = j
                 # we are storing the previous index only
-            else:
-                sums[i] = arr[i]
 
     # getting the max sum Idx
     currentMaxIdx = 0
@@ -18,11 +16,18 @@ def MaxSumSubsequence(arr):
         if sums[i] > sums[currentMaxIdx]:
             currentMaxIdx = i
 
-    print(currentMaxIdx)
-    print(sums)
-    print(sequence)
+    return GiveThoseNums(sums, currentMaxIdx, sequence)
+
+
+def GiveThoseNums(sums, idx, seq):
+    sequences = []
+    while idx is not None:
+        sequences.append(idx)
+        idx = seq[idx]
+    return list(reversed(sequences))
 
 
 if __name__ == "__main__":
     arr = [8, 12, 2, 3, 15, 5, 7]
-    MaxSumSubsequence(arr)
+    x = MaxSumSubsequence(arr)
+    print("We had to add nums of these indices to get max subsequence : ", x)
