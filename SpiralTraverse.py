c707f0361e1002d5109edf4451815f2e372e953f
Bishal Poudel <poudelb172@gmail.com>
2022-05-04 10:48:24 +0545

    Spiral Traverse By Iteration

---
+++ SpiralTraverse.py
@@ -0,0 +1,32 @@
+# By Iteration
+def SpiralTraverse(arr):
+    sr = 0
+    sc = 0
+    er = len(arr)-1
+    ec = len(arr[0])-1
+    ans = []
+
+    while sr < er or sc < ec:
+        for i in range(sc, ec+1):
+            ans.append(arr[sr][i])
+        for i in range(sr+1, er+1):
+            ans.append(arr[i][ec])
+        for i in reversed(range(sc, ec)):
+            ans.append(arr[er][i])
+        for i in reversed(range(sr+1, er)):
+            ans.append(arr[i][sr])
+        sr = sr+1
+        sc = sc+1
+        er = er-1
+        ec = ec-1
+
+    return ans
+
+
+if __name__ == "__main__":
+    givenArray = [[1, 2, 3, 4],
+                  [12, 13, 14, 5],
+                  [11, 16, 15, 6],
+                  [10, 9, 8, 7]]
+    x = SpiralTraverse(givenArray)
+    print(x)
