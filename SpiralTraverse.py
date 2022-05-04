cadee7cf9eded1871b9f0335c166ecc402b9d608
Bishal Poudel <poudelb172@gmail.com>
2022-05-04 10:49:06 +0545

    Spiral Traverse By Recursion

--- SpiralTraverse.py
+++ SpiralTraverse.py
@@ -30,3 +30,33 @@ if __name__ == "__main__":
                   [10, 9, 8, 7]]
     x = SpiralTraverse(givenArray)
     print(x)
+
+
+# By Recursion
+def Spiral(arr):
+    ans = []
+    SpiralTraverse(arr, 0, 0, len(arr)-1, len(arr[0])-1, ans)
+    return ans
+
+
+def SpiralTraverse(arr, sr, sc, er, ec, ans):
+    if sr > er and sc > ec:
+        return
+    for i in range(sc, ec+1):
+        ans.append(arr[sr][i])
+    for i in range(sr+1, er+1):
+        ans.append(arr[i][ec])
+    for i in reversed(range(sc, ec)):
+        ans.append(arr[er][i])
+    for i in reversed(range(sr+1, er)):
+        ans.append(arr[i][sr])
+    SpiralTraverse(arr, sr+1, sc+1, er-1, ec-1, ans)
+
+
+if __name__ == "__main__":
+    arr = [[1, 2, 3, 4],
+           [12, 13, 14, 5],
+           [11, 16, 15, 6],
+           [10, 9, 8, 7]]
+    x = Spiral(arr)
+    print(x)
