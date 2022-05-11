2eab6cf53c519d7a9d94334cba86341891e21551
Bishal Poudel <poudelb172@gmail.com>
2022-05-11 20:59:46 +0545

    NoOfWaysToTraverse : DynamicProgramming

--- NoOfWaysToTraverseGraph.py
+++ NoOfWaysToTraverseGraph.py
@@ -9,3 +9,24 @@ def NoOfWaysToTraverseGraph(width, height):
 if __name__ == "__main__":
     x = NoOfWaysToTraverseGraph(4, 3)
     print(x)
+
+
+
+# a better approach - BY USING DYNAMIC PROGRAMMING
+# Time and Space complexity O(n*m)
+def NoOfWaysToTraverseGraph(width, height):
+    arr = [[0 for i in range(width+1)] for i in range(height+1)]
+
+    for i in range(1, width+1):
+        for j in range(1, height+1):
+            if i == 1 or j == 1:
+                arr[j][i] = 1
+            else:
+                arr[j][i] = arr[j-1][i]+arr[j][i-1]
+
+    return arr[height][width]
+
+
+if __name__ == "__main__":
+    x = NoOfWaysToTraverseGraph(4, 3)
+    print(x)
