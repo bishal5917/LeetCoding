a4fabd600eee012eee16cdeca2c3320fc0dc1b2e
Bishal Poudel <poudelb172@gmail.com>
2022-05-18 16:24:10 +0545

    Staircase Traversal : Recursion+Memoization

--- StaircaseTraversal.py
+++ StaircaseTraversal.py
@@ -16,3 +16,22 @@ if __name__ == "__main__":
     print(StaircaseTraversal(height, maxsteps))
 
 
+# O(k*n) time where k=height and n=maxsteps and O(n) space
+def StaircaseTraversal(height, maxsteps, memoize):
+    if height in memoize:
+        return memoize[height]
+
+    noOfWays = 0
+    for step in range(1, min(maxsteps, height)+1):
+        noOfWays += StaircaseTraversal(height-step, maxsteps, memoize)
+
+    memoize[height] = noOfWays
+
+    return noOfWays
+
+
+if __name__ == "__main__":
+    memo = {0: 1, 1: 1}
+    height = 4
+    maxsteps = 2
+    print(StaircaseTraversal(height, maxsteps, memo))
