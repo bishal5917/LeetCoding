d55774495839bd88fd075c2b1cfc71ca4f9c8dc4
bishal5917 <poudelb172@gmail.com>
2022-11-05 13:34:12 +0545

    sudoku solver

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,7 +1,43 @@
 def SudokuSolver(board):
-    return checkHorizontally(board, 0, 2, 5), checkVertically(board, 2, 0, 5)
+    if solvePartialSudoku(board, 0, 0):
+        return board
+    else:
+        return "Cant Solve"
 
 
+def solvePartialSudoku(board, row, col):
+    currentRow = row
+    currentCol = col
+
+    if currentCol == len(board[0]) - 1:
+        currentRow += 1
+        currentCol = 0
+        if currentRow == len(board):
+            return True
+            # that means we have solved till the last one,so returns true
+
+    if board[currentRow][currentCol] == 0:
+        return TryPlacingDigits(board, currentRow, currentCol)
+
+    return solvePartialSudoku(board, currentRow, currentCol + 1)
+
+
+def TryPlacingDigits(board, Row, Col):
+    for i in range(1, 10):
+        if (
+            checkHorizontally(board, Row, Col, i)
+            and checkVertically(board, Row, Col, i)
+            and checkSubGrid(board, Row, Col, i)
+        ):
+            board[Row][Col] = i
+        if solvePartialSudoku(board, Row, Col + 1):
+            return True
+
+    board[Row][Col] = 0
+    return False
+
+
+# 3 checker functions
 def checkHorizontally(board, i, j, num):
     # if return boolean is true we can place that element
     for item in board[i]:
@@ -9,12 +45,26 @@ def checkHorizontally(board, i, j, num):
             return False
     return True
 
-    
+
 def checkVertically(board, i, j, num):
     # if return boolean is true we can place that element
     for items in board:
         if items[i] == num:
             return False
+    return True
+
+
+def checkSubGrid(board, i, j, num):
+    # if return boolean is true we can place that element
+    subgridRowStart = i // 3
+    subgridColStart = j // 3
+    for rowidx in range(3):
+        for colidx in range(3):
+            rowToCheck = subgridRowStart * 3 + rowidx
+            colToCheck = subgridColStart * 3 + colidx
+            currVal = board[rowToCheck][colToCheck]
+            if num == currVal:
+                return False
     return True
 
 
