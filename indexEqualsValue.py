8d501d0a7646e36063903870ac98f9c08bffa03e
bishal5917 <poudelb172@gmail.com>
2022-11-06 10:36:20 +0545

    IndexEqualsValue Optimal Solution O(log(N)) time

--- indexEqualsValue.py
+++ indexEqualsValue.py
@@ -5,14 +5,24 @@ def IndexEqualsValue(arr):
             return i
 
 
-# O(log(N)) Time and O(1) Space
+# O(log(N)) Time and O(log(N)) Space
+# If Done by iteration this could have been in O(log(N)) time and O(1) space
 # Note : Given Array should be sorted
-def IndexEqualsValueOptimal(arr):
-    for i in range(0, len(arr)):
-        if i == arr[i]:
-            return i
+def IndexEqualsValueOptimal(arr, left, right):
+    if left > right:
+        return 0
+    mid = (left + right) // 2
+    if arr[mid] == mid and mid == 0:
+        return mid
+    if arr[mid] == mid and arr[mid - 1] < mid - 1:
+        return mid
+    elif arr[mid] < mid:
+        return IndexEqualsValueOptimal(arr, mid + 1, right)
+    elif arr[mid] > mid:
+        return IndexEqualsValueOptimal(arr, left, mid - 1)
 
 
 if __name__ == "__main__":
-    arr = [-5, -3, 0, 3, 4, 7, 9]
-    # print(IndexEqualsValue(arr))
+    arr = [-5, -3, 0, 1, 2, 4, 6]
+    print(IndexEqualsValue(arr))
+    print(IndexEqualsValueOptimal(arr, 0, len(arr) - 1))
