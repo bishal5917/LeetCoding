67934ad180565e45dfac8f1cf744e18a2582597c
bishal5917 <poudelb172@gmail.com>
2022-11-14 19:29:35 +0545

    word search problem

--- WordSearch.py
+++ WordSearch.py
@@ -1,60 +1,68 @@
-def WordSearch(board, word):
-    idxs = getIdxOfFirstLetter(board, word)
+class Solution(object):
+    def exist(self, board, word):
+        idxs = self.getIdxOfFirstLetter(board, word)
 
-    for item in idxs:
-        wordLocs = [item]
+        for item in idxs:
+            wordLocs = [item]
 
-        i = 0
-        neighbs = getNeighbors(item[0], item[1], board)
+            i = 0
+            neighbs = self.getNeighbors(item[0], item[1], board)
 
-        while i < len(word):
-            # return neighbs
-            for item in neighbs:
-                if board[item[0]][item[1]] == word[i]:
-                    # print(i)
-                    wordLocs.append([item[0], item[1]])
-                    neighbs = getNeighbors(item[0], item[1], board)
-                    print(neighbs)
-                else:
-                    continue
-            i += 1
+            while i < len(word):
+                for item in neighbs:
+                    if (
+                        board[item[0]][item[1]] == word[i]
+                        and self.checkIfAlreadyVisited(wordLocs, item[0], item[1])
+                        == False
+                    ):
+                        wordLocs.append([item[0], item[1]])
+                        neighbs = self.getNeighbors(item[0], item[1], board)
+                        # print(neighbs)
+                    else:
+                        continue
+                i += 1
 
-        if len(wordLocs) == len(word):
-            return wordLocs, True
-        else:
-            continue
+            if len(wordLocs) == len(word):
+                # return wordLocs, True
+                return True
+            else:
+                continue
 
-    return False
+        return wordLocs,False
+
+    def getIdxOfFirstLetter(self, board, word):
+        idxs = []
+        firstLetter = word[0]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == firstLetter:
+                    idxs.append([i, j])
+        return idxs
+
+    def checkIfAlreadyVisited(self, visitedIdxs, row, col):
+        for item in visitedIdxs:
+            if item[0] == row and item[1] == col:
+                return True
+        return False
+
+    def getNeighbors(self, row, col, board):
+        neighbors = []
+        if (col + 1) < len(board[0]):
+            neighbors.append([row, col + 1])
+        if (col - 1) >= 0:
+            neighbors.append([row, col - 1])
+        if (row - 1) >= 0:
+            neighbors.append([row - 1, col])
+        if (row + 1) < len(board):
+            neighbors.append([row + 1, col])
+
+        return neighbors
 
 
-def getIdxOfFirstLetter(board, word):
-    idxs = []
-    firstLetter = word[0]
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == firstLetter:
-                idxs.append([i, j])
-    return idxs
-
-
-def getNeighbors(row, col, board):
-    neighbors = []
-    if (col + 1) < len(board[0]):
-        neighbors.append([row, col + 1])
-    if (col - 1) >= 0:
-        neighbors.append([row, col - 1])
-    if (row - 1) >= 0:
-        neighbors.append([row - 1, col])
-    if (row + 1) < len(board):
-        neighbors.append([row + 1, col])
-
-    return neighbors
-
-
-if __name__ == "__main__":
-    # board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-    # word = "ABCCED"
-    # word = "ADEE"
-    board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-    word = "ABCB"
-    print(WordSearch(board, word))
+# board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+# word = "ABCCED"
+# word = "ADEE"
+board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+word = "SEE"
+obj = Solution()
+print(obj.exist(board, word))
