659771a9523d6cfaf926ccf98be0b047fa686aee
bishal5917 <poudelb172@gmail.com>
2022-12-01 17:23:28 +0545

    get and merge substring locations

--- UnderscorifySubstring.py
+++ UnderscorifySubstring.py
@@ -1,19 +1,29 @@
 def UnderscorifySubstring(str, substr):
-    getLocations = [[0, 4], [10, 14], [14, 18], [20, 24], [23, 27], [26, 30]]
+    # code to get locations of substring on main string
+    locations = []
+    startIdx = 0
+    while startIdx < len(str):
+        nextIdx = str.find(substr, startIdx)
+        if nextIdx != -1:
+            locations.append([nextIdx, nextIdx + len(substr)])
+            startIdx = nextIdx + 1
+        else:
+            break
 
-    
-    # code to overlap locations
-    mergedLocations = [getLocations[0]]
+    # code to overlap locations if any
+    mergedLocations = [locations[0]]
     previous = mergedLocations[0]
-    for i in range(1, len(getLocations)):
-        current = getLocations[i]
+    for i in range(1, len(locations)):
+        current = locations[i]
         if current[0] <= previous[1]:
             previous[1] = current[1]
         else:
             mergedLocations.append(current)
             previous = current
-    print(mergedLocations)
 
+    # print(locations)
+    # print(mergedLocations)
+    
 
 if __name__ == "__main__":
     str = "testthis is a testtest to see if testestest it works"
