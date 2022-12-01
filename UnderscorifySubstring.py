64ec5bff11054825bbe96163e94cd9484b1cd460
bishal5917 <poudelb172@gmail.com>
2022-12-01 10:39:45 +0545

    merge location attempt

--- UnderscorifySubstring.py
+++ UnderscorifySubstring.py
@@ -1,9 +1,15 @@
 def UnderscorifySubstring(str, substr):
-    getLocations = []
+    getLocations = [[0, 4], [10, 14], [14, 18], [20, 24], [23, 27], [26, 30]]
     # code to overlap locations
-    
+    # for x in range(2):
+    for i in range(1, len(getLocations) - 2):
+        if getLocations[i][0] <= getLocations[i - 1][1]:
+            getLocations[i - 1][1] = getLocations[i][1]
+            getLocations.pop(i)
+    print(getLocations)
 
 
 if __name__ == "__main__":
     str = "testthis is a testtest to see if testestest it works"
     substr = "test"
+    UnderscorifySubstring(str, substr)
