f2f7d447ec6b46663175da2bc45eb7938f732fd9
bishal5917 <poudelb172@gmail.com>
2022-12-01 10:05:12 +0545

    UnderscorifySubstring

---
+++ UnderscorifySubstring.py
@@ -0,0 +1,9 @@
+def UnderscorifySubstring(str, substr):
+    getLocations = []
+    # code to overlap locations
+    
+
+
+if __name__ == "__main__":
+    str = "testthis is a testtest to see if testestest it works"
+    substr = "test"
