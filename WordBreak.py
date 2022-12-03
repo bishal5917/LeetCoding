6d7f2df91fe68cee8ff3c71daeb36ca043bf4165
bishal5917 <poudelb172@gmail.com>
2022-12-03 10:55:36 +0545

    word break leetcode problem

--- WordBreak.py
+++ WordBreak.py
@@ -1,2 +1,19 @@
+def WordBreak(s, wordDict):
+    dp = [False] * (len(s) + 1)
+    dp[len(s)] = True
 
-def WordBreak()
+    for i in range(len(s) - 1, -1, -1):
+        for item in wordDict:
+            if (i + len(item)) <= len(s) and s[i : i + len(item)] == item:
+                dp[i] = dp[i + len(item)]
+
+            if dp[i]:
+                break
+
+    return dp[0]
+
+
+if __name__ == "__main__":
+    s = "leetcode"
+    wordDict = ["leet", "code"]
+    print(WordBreak(s, wordDict))
