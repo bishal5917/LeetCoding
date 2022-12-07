384c0846ddd9e6fdb7237393234e63e9885238c9
bishal5917 <poudelb172@gmail.com>
2022-12-07 18:08:52 +0545

    Game of life leetcode problem solved 😎

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,32 +1,50 @@
 class Solution(object):
     def gameOfLife(self, board):
-        neighs = self.getNeighbors(1, 0, board)
-        print(neighs)
+        # DONOT RETURN ANYTHING : MODIFY THE ORIGINAL BOARD
+        final = [[0 for i in range(0, len(board[0]))] for j in range(0, len(board))]
 
-    def getNeighbors(self, row, col, graph):
+        for i in range(0, len(board)):
+            for j in range(0, len(board[0])):
+                getNeighs = self.getAllNeighbors(i, j, board)
+                if board[i][j] == 0 and sum(getNeighs) == 3:
+                    final[i][j] = 1
+                if board[i][j] == 1 and sum(getNeighs) < 2:
+                    final[i][j] = 0
+                if board[i][j] == 1 and sum(getNeighs) == 2 or sum(getNeighs) == 3:
+                    final[i][j] = 1
+                if board[i][j] == 1 and sum(getNeighs) > 3:
+                    final[i][j] = 0
+
+        for i in range(0, len(board)):
+            for j in range(0, len(board[0])):
+                board[i][j] = final[i][j]
+
+    def getAllNeighbors(self, row, col, graph):
         neighbors = []
         if (col + 1) <= len(graph[0]) - 1:
-            neighbors.append([row, col + 1])
+            neighbors.append(graph[row][col + 1])
         if (col - 1) >= 0:
-            neighbors.append([row, col - 1])
+            neighbors.append(graph[row][col - 1])
         if (row - 1) >= 0:
-            neighbors.append([row - 1, col])
+            neighbors.append(graph[row - 1][col])
         if (row + 1) <= len(graph) - 1:
-            neighbors.append([row + 1, col])
+            neighbors.append(graph[row + 1][col])
         # FOR DIAGONAL NEIGHBOURS
         if (row - 1) >= 0 and (col - 1) >= 0:
-            neighbors.append([row - 1, col - 1])
+            neighbors.append(graph[row - 1][col - 1])
         if (row - 1) >= 0 and (col + 1) <= len(graph[0]) - 1:
-            neighbors.append([row - 1, col + 1])
+            neighbors.append(graph[row - 1][col + 1])
         if (row + 1) <= len(graph) - 1 and (col - 1) >= 0:
-            neighbors.append([row + 1, col - 1])
+            neighbors.append(graph[row + 1][col - 1])
         if (row + 1) <= len(graph) - 1 and (col + 1) <= len(graph[0]) - 1:
-            neighbors.append([row + 1, col + 1])
+            neighbors.append(graph[row + 1][col + 1])
 
         return neighbors
 
 
 if __name__ == "__main__":
-    graph = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+    board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+    # board = [[1, 1], [1, 0]]
     obj = Solution()
-    print(obj.gameOfLife(graph))
+    print(obj.gameOfLife(board))
+    print(board)
