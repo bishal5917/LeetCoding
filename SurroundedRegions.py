b8d24c354debdc58577961fad3f021e5ee4d33bf
bishal5917 <poudelb172@gmail.com>
2022-12-08 12:05:21 +0545

    Surrounded Regions LeetCode Problem Solved Finally 😎🔥

--- SurroundedRegions.py
+++ SurroundedRegions.py
@@ -1,62 +1,3 @@
-# class Solution(object):
-#     def solve(self, board):
-#         zeroesPos = self.getZeroesPos(board)
-
-#         for poss in zeroesPos:
-#             neighs = self.getNeighbors(poss[0], poss[1], board)
-#             if len(neighs) == 4 and self.checkValidToALter(neighs, board):
-#                 board[poss[0]][poss[1]] = "X"
-
-#         return board
-
-#     def checkValidToALter(self, neighs, board):
-#         borders = self.getBorders(board)
-#         zeroesPos = self.getZeroesPos(board)
-#         for item in neighs:
-#             if item in borders and item in zeroesPos:
-#                 return False
-
-#         return True
-
-#     def getZeroesPos(self, board):
-#         zeroesPos = []
-#         for i in range(0, len(board)):
-#             for j in range(0, len(board[0])):
-#                 if board[i][j] == "O":
-#                     zeroesPos.append([i, j])
-#         return zeroesPos
-
-#     def getNeighbors(self, row, col, graph):
-#         neighbors = []
-#         if (col + 1) <= len(graph[0]) - 1:
-#             neighbors.append([row, col + 1])
-#         if (col - 1) >= 0:
-#             neighbors.append([row, col - 1])
-#         if (row - 1) >= 0:
-#             neighbors.append([row - 1, col])
-#         if (row + 1) <= len(graph) - 1:
-#             neighbors.append([row + 1, col])
-#         return neighbors
-
-#     def getBorders(self, board):
-#         sr = 0
-#         sc = 0
-#         er = len(board) - 1
-#         ec = len(board[0]) - 1
-#         borders = []
-
-#         for i in range(sc, ec + 1):
-#             borders.append([sr, i])
-#         for i in range(sr + 1, er + 1):
-#             borders.append([i, ec])
-#         for i in reversed(range(sc, ec)):
-#             borders.append([er, i])
-#         for i in reversed(range(sr + 1, er)):
-#             borders.append([i, sr])
-
-#         return borders
-
-
 class Solution(object):
     def solve(self, board):
         borders = self.getBorders(board)
@@ -64,16 +5,16 @@ class Solution(object):
             if board[item[0]][item[1]] == "O":
                 board[item[0]][item[1]] = "T"
 
-        for i in range(0, len(board)):
-            for j in range(0, len(board[0])):
-                getneighs = self.getNeighbors(i, j, board)
-                for item in getneighs:
-                    if (
-                        board[i][j] == "O"
-                        and board[item[0]][item[1]] == "O"
-                        and item in borders
-                    ):
-                        board[i][j] = "T"
+        visited = []
+
+        for item in borders:
+            if board[item[0]][item[1]] == "T" and [item[0], item[1]] not in visited:
+                visited.append([item[0], item[1]])
+                self.BFSInGrid(board, visited, item[0], item[1])
+
+        for item in visited:
+            if board[item[0]][item[1]] == "O":
+                board[item[0]][item[1]] = "T"
 
         for i in range(0, len(board)):
             for j in range(0, len(board[0])):
@@ -85,6 +26,19 @@ class Solution(object):
                 if board[i][j] == "T":
                     board[i][j] = "O"
         return board
+
+    def BFSInGrid(self, grid, visited, i, j):
+        queue = [[i, j]]
+
+        while queue:
+            first = queue.pop(0)
+            allneighs = self.getNeighbors(first[0], first[1], grid)
+            for item in allneighs:
+                if item not in visited and grid[item[0]][item[1]] == "O":
+                    visited.append(item)
+                    queue.append(item)
+
+        return visited
 
     def getNeighbors(self, row, col, graph):
         neighbors = []
@@ -125,7 +79,6 @@ if __name__ == "__main__":
         ["X", "O", "X", "X"],
     ]
 
-    board = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]
+    # board = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]
     obj = Solution()
     print(obj.solve(board))
-    # print(obj.getBorders(board))
