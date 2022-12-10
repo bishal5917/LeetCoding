920693c9b20a8b0c318413378e1378c11dcc7002
bishal5917 <poudelb172@gmail.com>
2022-12-10 14:29:01 +0545

    Word Search Problem Solved

--- WordSearch.py
+++ WordSearch.py
@@ -1,66 +1,42 @@
 class Solution(object):
     def exist(self, board, word):
-        idxs = self.getIdxOfFirstLetter(board, word)
+        rows, cols = len(board), len(board[0])
+        visited = []
 
-        for item in idxs:
-            wordLocs = [item]
-
-            i = 0
-            neighbs = self.getNeighbors(item[0], item[1], board)
-
-            while i < len(word):
-                for item in neighbs:
-                    if (
-                        board[item[0]][item[1]] == word[i]
-                        and self.checkIfAlreadyVisited(wordLocs, item[0], item[1])
-                        == False
-                    ):
-                        wordLocs.append([item[0], item[1]])
-                        neighbs = self.getNeighbors(item[0], item[1], board)
-                        # print(neighbs)
-                    else:
-                        continue
-                i += 1
-
-            if len(wordLocs) == len(word):
-                # return wordLocs, True
-                return wordLocs,True
-            else:
-                continue
-
-        return wordLocs,False
-
-    def getIdxOfFirstLetter(self, board, word):
-        idxs = []
-        firstLetter = word[0]
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == firstLetter:
-                    idxs.append([i, j])
-        return idxs
-
-    def checkIfAlreadyVisited(self, visitedIdxs, row, col):
-        for item in visitedIdxs:
-            if item[0] == row and item[1] == col:
+        def DFS(r, c, i):
+            if i == len(word):
                 return True
+            if (
+                r < 0
+                or c < 0
+                or r >= rows
+                or c >= cols
+                or word[i] != board[r][c]
+                or [r, c] in visited
+            ):
+                return False
+            visited.append([r, c])
+            if DFS(r + 1, c, i + 1):
+                return True
+            if DFS(r - 1, c, i + 1):
+                return True
+            if DFS(r, c + 1, i + 1):
+                return True
+            if DFS(r, c - 1, i + 1):
+                return True
+            visited.remove([r, c])
+            return False
+
+        for i in range(rows):
+            for j in range(cols):
+                if DFS(i, j, 0):
+                    return True
+
         return False
-
-    def getNeighbors(self, row, col, board):
-        neighbors = []
-        if (col + 1) < len(board[0]):
-            neighbors.append([row, col + 1])
-        if (col - 1) >= 0:
-            neighbors.append([row, col - 1])
-        if (row - 1) >= 0:
-            neighbors.append([row - 1, col])
-        if (row + 1) < len(board):
-            neighbors.append([row + 1, col])
-
-        return neighbors
 
 
 board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-word = "ABCCED"
-# word = "ADEE"
+# word = "ABCCED"
+word = "ADEE"
 obj = Solution()
 print(obj.exist(board, word))
