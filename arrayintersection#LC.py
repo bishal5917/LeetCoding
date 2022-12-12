56d731904b319bc06ef2a08a7cd01fca5befe0c4
bishal5917 <poudelb172@gmail.com>
2022-12-12 21:15:38 +0545

    array intersection leetcode problem solved

--- arrayintersection#LC.py
+++ arrayintersection#LC.py
@@ -1,17 +1,18 @@
 def intersect(nums1, nums2):
     final = []
     i, j = 0, 0
+    nums1.sort()
+    nums2.sort()
 
     while i < len(nums1) and j < len(nums2):
         if nums1[i] == nums2[j]:
             final.append(nums1[i])
             i += 1
             j += 1
-        elif nums1[i] != nums2[j]:
-            if len(nums2) > len(nums1):
-                j += 1
-            else:
-                i += 1
+        elif nums1[i] < nums2[j]:
+            i += 1
+        elif nums1[i] > nums2[j]:
+            j += 1
 
     return final
 
