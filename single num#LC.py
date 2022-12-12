6963ff5d325d5cf886920139d6cfdfafdfa712e7
bishal5917 <poudelb172@gmail.com>
2022-12-12 18:04:15 +0545

    single number Leetcode problem solved

--- single num#LC.py
+++ single num#LC.py
@@ -1,15 +1,10 @@
-nums = [2, 2, 4, 4, 1, 1, 1, 65, 65, 2, 45]
-# we have to find the number that appears exactly one time
-# here is 45
+def singleNumber(nums):
+    result = 0
+    for item in nums:
+        result = result ^ item
+    return result
 
-list = []
-# making a list with all the numbers without repeating
-for num in nums:
-    if num not in list:
-        list.append(num)
 
-# checking the count of numbers of our list in original given list
-for item in list:
-    # printing if count is exactly one
-    if nums.count(item) == 1:
-        print(item)
+nums = [4, 1, 2, 1, 2]
+print(singleNumber(nums))
+# XOR operation done (^)
