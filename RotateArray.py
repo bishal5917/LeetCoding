c2c86867aeebe017484d17f3807124924a48b5f9
bishal5917 <poudelb172@gmail.com>
2022-12-20 21:41:57 +0545

    Rotate Array Optimal Solution

--- RotateArray.py
+++ RotateArray.py
@@ -1,14 +1,26 @@
-def RotateArray(nums, k):
-    for _ in range(k):
-        last = nums[-1]
-        for i in range(0, len(nums)):
-            nums[len(nums) - 1 - i] = nums[len(nums) - 2 - i]
-        nums[0] = last
+# This isn't the optimal solution
+# def RotateArray(nums, k):
+#     for _ in range(k):
+#         last = nums[-1]
+#         for i in range(0, len(nums)):
+#             nums[len(nums) - 1 - i] = nums[len(nums) - 2 - i]
+#         nums[0] = last
 
-    return nums
+#     return nums
+
+
+def RotateArray(nums, k):
+    temp = [0 for _ in nums]
+
+    for i in range(0, len(nums)):
+        pos = (i + k) % len(nums)
+        temp[pos] = nums[i]
+
+    for i in range(0, len(nums)):
+        nums[i] = temp[i]
 
 
 if __name__ == "__main__":
-    nums = [1, 2, 3, 4, 5, 6, 7]
-    k = 3
+    nums = [1, 2, 3, 4, 5]
+    k = 2
     print(RotateArray(nums, k))
