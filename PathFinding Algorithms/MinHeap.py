288da858888f6d2b8c54f7e91cb7f91af0b92830
bishal5917 <poudelb172@gmail.com>
2023-01-03 13:48:57 +0545

    MinHeap : Insert Method Implemented

--- PathFinding Algorithms/MinHeap.py
+++ PathFinding Algorithms/MinHeap.py
@@ -1,18 +1,27 @@
 class MinHeap:
     def __init__(self, arr):
-        self.heap = self.buildHeap(self, arr)
+        self.heap = self.buildHeap(arr)
 
     def peek(self):
         return self.heap[0]
 
     def insert(self, value):
-        pass
+        self.heap.append(value)
+        lastIdx = len(self.heap) - 1
+        parentIdx = (lastIdx - 1) // 2
+        while True:
+            if self.heap[parentIdx] > value:
+                self.swap(parentIdx, lastIdx, self.heap)
+            lastIdx = parentIdx
+            parentIdx = (parentIdx - 1) // 2
+            if not parentIdx:
+                return self.heap
 
     def remove(self):
         pass
 
     def buildHeap(self, arr):
-        pass
+        return arr
 
     def siftDown(self, currIdx, endIdx, heap):
         pass
@@ -22,3 +31,11 @@ class MinHeap:
 
     def swap(self, i, j, heap):
         heap[i], heap[j] = heap[j], heap[i]
+
+
+if __name__ == "__main__":
+    arr = [8, 12, 23, 17, 31, 30, 44, 102, 18]
+    x = MinHeap(arr)
+    print(x.peek())
+    print(x.insert(9))
+    print(x.remove())
