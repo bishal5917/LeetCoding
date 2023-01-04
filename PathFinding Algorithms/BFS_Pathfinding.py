a16a0d912a7932c96e9f24c34592ec142f5f629e
bishal5917 <poudelb172@gmail.com>
2023-01-04 19:36:36 +0545

    BFS PathFinding Successfully Implemented !!!

--- PathFinding Algorithms/BFS_Pathfinding.py
+++ PathFinding Algorithms/BFS_Pathfinding.py
@@ -2,6 +2,7 @@ def BFSInGrid(grid, start, end):
 
     visited = [start]
     queue = [start]
+    prev = [start]
 
     while queue:
         first = queue.pop(0)
@@ -10,11 +11,37 @@ def BFSInGrid(grid, start, end):
             if item not in visited and grid[item[0]][item[1]] == 1:
                 queue.append(item)
                 visited.append(item)
+                prev.append(first)
             if grid[item[0]][item[1]] == "E":
                 visited.append(end)
-                return visited
+                prev.append(first)
 
-    return -1
+    # Getting the shortest path
+    shortestPath = []
+    shortestPath.append(visited[-1])
+    shortestPath.append(prev[-1])
+    while True:
+        lastOne = shortestPath[-1]
+        idx = visited.index(lastOne)
+        shortestPath.append(prev[idx])
+        if lastOne == start:
+            shortestPath.pop()
+            if shortestPath[0] != end:
+                return False
+            return shortestPath
+
+
+def MakePath(grid, path):
+    for item in path:
+        if grid[item[0]][item[1]] == 1:
+            grid[item[0]][item[1]] = "-"
+
+
+def printMatrix(matrix):
+    for item in matrix:
+        for element in item:
+            print(element, end="\t")
+        print("")
 
 
 def getNeighbors(row, col, graph):
@@ -33,15 +60,13 @@ def getNeighbors(row, col, graph):
 def GenerateMaze(grid):
     import random
 
-    noOfObstacles = (len(grid) * len(grid[0])) // 2
+    noOfObstacles = (len(grid) * len(grid[0])) // 3
 
     for _ in range(0, noOfObstacles):
         one = random.randint(0, len(grid) - 1)
         two = random.randint(0, len(grid[0]) - 1)
         if grid[one][two] == 1:
             grid[one][two] = "#"
-
-    print(grid)
 
 
 if __name__ == "__main__":
@@ -50,9 +75,42 @@ if __name__ == "__main__":
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
         [1, 1, 1, 1, "E"],
     ]
     start = [0, 1]
     end = [4, 4]
+    # testGridA = [
+    #     [1, "S", 1, 1, 1],
+    #     [1, "#", "#", "#", 1],
+    #     [1, 1, 1, 1, 1],
+    #     ["#", 1, "#", "#", "#"],
+    #     [1, 1, 1, "E", 1],
+    # ]
+    # start = [0, 1]
+    # end = [4, 3]
+    # testGridB = [
+    #     [1, "S", 1, 1, 1],
+    #     ["#", 1, 1, "#", "#"],
+    #     ["#", 1, "#", "#", 1],
+    #     [1, 1, 1, 1, 1],
+    #     ["#", "#", "#", "#", "E"],
+    # ]
+    # start = [0, 1]
+    # end = [4, 4]
     GenerateMaze(grid)
-    print(BFSInGrid(grid, start, end))
+    printMatrix(grid)
+    print("")
+    pathPlots = BFSInGrid(grid, start, end)
+    if pathPlots != False:
+        MakePath(grid, pathPlots)
+        printMatrix(grid)
+    else:
+        print("No Path Found")
