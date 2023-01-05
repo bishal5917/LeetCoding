95463430c0e2de754ece58fefb7ef76ea1fa4c17
bishal5917 <poudelb172@gmail.com>
2023-01-05 20:15:07 +0545

    MinHeap Modified Accordingly !!!

--- PathFinding Algorithms/A_star_Algorithm.py
+++ PathFinding Algorithms/A_star_Algorithm.py
@@ -19,6 +19,7 @@ def aStarAlgo(startRow, startCol, endRow, endCol, graph):
     # print(startNode.f_score)
 
     # Now getting the MinHeap
+    nodesToVisit = MinHeap([startNode])
 
 
 def calculateHeuristic(startNode, endNode):
@@ -42,6 +43,75 @@ def initializeNodes(graph):
             nodes[i].append(Node(i, j, value))
 
     return nodes
+
+
+# MinHeap Class
+class MinHeap:
+    def __init__(self, array):
+        # Holds the position in the heap that each node is at
+        self.nodePositionsInHeap = {node.id: idx for idx, node in enumerate(array)}
+        self.heap = self.buildHeap(array)
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
+    def buildHeap(self, array):
+        firstParentIdx = (len(array) - 2) // 2
+        for currentIdx in reversed(range(firstParentIdx + 1)):
+            self.siftDown(currentIdx, len(array) - 1, array)
+        return array
+
+    def siftDown(self, currentIdx, endIdx, heap):
+        childOneIdx = currentIdx * 2 + 1
+        while childOneIdx <= endIdx:
+            childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 <= endIdx else -1
+            if (
+                childTwoIdx != -1
+                and heap[childTwoIdx].f_score < heap[childOneIdx].f_score
+            ):
+                idxToSwap = childTwoIdx
+            else:
+                idxToSwap = childOneIdx
+
+            if heap[idxToSwap].f_score < heap[currentIdx].f_score:
+                self.swap(currentIdx, idxToSwap, heap)
+                currentIdx = idxToSwap
+                childOneIdx = currentIdx * 2 + 1
+            else:
+                return
+
+    def siftUp(self, currentIdx, heap):
+        parentIdx = (currentIdx - 1) // 2
+        while currentIdx > 0 and heap[currentIdx].f_score < heap[parentIdx].f_score:
+            self.swap(currentIdx, parentIdx, heap)
+            currentIdx = parentIdx
+            parentIdx = (currentIdx - 1) // 2
+
+    def remove(self):
+        if self.isEmpty():
+            return
+
+        self.swap(0, len(self.heap) - 1, self.heap)
+        node = self.heap.pop()
+        del self.nodePositionsInHeap[node.id]
+        self.siftDown(0, len(self.heap) - 1, self.heap)
+        return node
+
+    def insert(self, node):
+        self.heap.append(node)
+        self.nodePositionsInHeap[node.id] = len(self.heap) - 1
+        self.siftUp(len(self.heap) - 1, self.heap)
+
+    def swap(self, i, j, heap):
+        self.nodePositionsInHeap[heap[i].id] = j
+        self.nodePositionsInHeap[heap[j].id] = i
+        heap[i], heap[j] = heap[j], heap[i]
+
+    def containsNode(self, node):
+        return node.id in self.nodePositionsInHeap
+
+    def update(self, node):
+        self.siftUp(self.nodePositionsInHeap[node.id], self.heap)
 
 
 if __name__ == "__main__":
