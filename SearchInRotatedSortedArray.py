2ebf51657bf1470834ce96400f359a03f0f87b48
bishal5917 <poudelb172@gmail.com>
2023-04-22 13:05:26 +0545

    search rotated array

--- SearchInRotatedSortedArray.py
+++ SearchInRotatedSortedArray.py
@@ -4,17 +4,26 @@ def SearchInRotatedSortedArray(nums, target):
 
     while left <= right:
         mid = (left + right) // 2
-        if nums[left] > target and nums[right] < target:
-            left = mid + 1
-        if nums[left] < target and nums[right] > target:
-            right = mid - 1
+
         if nums[mid] == target:
             return mid
+
+        if nums[left] <= nums[mid]:
+            if target > nums[mid] or target < nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        else:
+            if target < nums[mid] or target > nums[right]:
+                right = mid - 1
+            else:
+                left = mid + 1
 
     return -1
 
 
 if __name__ == "__main__":
     nums = [4, 5, 6, 7, 0, 1, 2]
-    target = 2
+    target = 0
     print(SearchInRotatedSortedArray(nums, target))
