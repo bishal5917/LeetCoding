608a1b89223345c38ab4abd023d3c4b315d808c2
bishal5917 <poudelb172@gmail.com>
2023-04-25 16:04:26 +0545

    Minimim Path Sum solved

---
+++ MinimumPathSum.py
@@ -0,0 +1,24 @@
+def minPathSum(grid):
+    final = [[0 for i in range(0, len(grid[0]))] for j in range(0, len(grid))]
+
+    for gidx in range(len(grid) - 1, -1, -1):
+        for i in range(len(grid[gidx]) - 1, -1, -1):
+            if gidx + 1 >= len(grid) and i + 1 >= len(grid[gidx]):
+                final[gidx][i] = grid[gidx][i]
+            if gidx + 1 >= len(grid) and i + 1 < len(grid[gidx]):
+                final[gidx][i] = grid[gidx][i] + final[gidx][i + 1]
+            if gidx + 1 < len(grid) and i + 1 >= len(grid[gidx]):
+                final[gidx][i] = final[gidx + 1][i] + grid[gidx][i]
+            if gidx + 1 < len(grid) and i + 1 < len(grid[gidx]):
+                final[gidx][i] = min(
+                    grid[gidx][i] + final[gidx][i + 1],
+                    grid[gidx][i] + final[gidx + 1][i],
+                )
+
+    return final[0][0]
+
+
+if __name__ == "__main__":
+    grid = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
+    grid = [[1, 2, 3], [4, 5, 6]]
+    print(minPathSum(grid))
