1fffc3baf86259d83a323edb65f231615a4efae5
bishal5917 <poudelb172@gmail.com>
2023-04-25 15:24:51 +0545

    Daily Temperatures Solved using Monotonic Stack

--- DailyTemperatures.py
+++ DailyTemperatures.py
@@ -1,23 +1,16 @@
 def dailyTemperatures(temperatures):
     output = [0 for _ in range(0, len(temperatures))]
+    stack = []
 
-    i = 0
-    for i in range(0, len(temperatures) - 1):
-        j = i + 1
-        if temperatures[j] > temperatures[i]:
-            output[i] = 1
-        if temperatures[j] <= temperatures[i]:
-            count = 1
-            for x in range(j, len(temperatures)):
-                if temperatures[x] <= temperatures[i]:
-                    count += 1
-                if temperatures[x] > temperatures[i]:
-                    output[i] = count
+    for idx,val in enumerate(temperatures):
+        while len(stack)>0 and stack[-1][1]<val:
+            sIdx,sVal=stack.pop()
+            output[sIdx]=idx-sIdx
+        stack.append([idx,val])
 
     return output
 
 
 if __name__ == "__main__":
     temps = [73, 74, 75, 71, 69, 72, 76, 73]
-    temps = [89, 62, 70, 58, 47, 47, 46, 76, 100, 70]
     print(dailyTemperatures(temps))
