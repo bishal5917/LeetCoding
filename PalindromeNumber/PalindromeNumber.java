2c66181f8e34a2a8d0e0e0e1ee670a261efb6e4c
bishal5917 <poudelb172@gmail.com>
2023-06-26 20:44:36 +0545

    palindrome number solved using java

---
+++ PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,24 @@
+
+class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+
+        String newX = Integer.toString(x);
+        int i = 0;
+        int j = newX.length() - 1;
+
+        while (i != j & i<newX.length() & j>=0) {
+            if (newX.charAt(i) == newX.charAt(j)) {
+                i += 1;
+                j -= 1;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber pd = new PalindromeNumber();
+        System.out.println(pd.isPalindrome(11));
+    }
+}
--- ThreeSum/ThreeSum.java
+++ ThreeSum/ThreeSum.java
@@ -1,4 +1,3 @@
-package ThreeSum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,20 +5,29 @@ import java.util.List;
 
 class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        result.add(Arrays.asList(1, 0, 0));
-        result.add(Arrays.asList(0, 0));
-
-        return result;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                for (int k = 2; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0 && nums[i] != nums[j] && nums[j] != nums[k]
+                            && nums[j] != nums[k]) {
+                        System.out.println("True");
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
 
-        List<Integer> arr = new ArrayList<>(Arrays.asList(-1, 0, 1, 2, -1, -4));
+        List<Integer> numsList = Arrays.asList(-1, 0, 1, 2, -1, -4);
+        int[] nums = new int[numsList.size()];
+        for (int i = 0; i < numsList.size(); i++) {
+            nums[i] = numsList.get(i);
+        }
 
-        System.out.println(arr.get(0)); // Accessing the first element of the list
+        ThreeSum tsum = new ThreeSum();
+        tsum.threeSum(nums);
 
     }
 }
