7b23ed0cccb38c450945a388ab72c60a6189ddcd
bishal5917 <poudelb172@gmail.com>
2023-07-02 21:04:47 +0545

    remove element

--- RemoveElement/Solution.java
+++ RemoveElement/Solution.java
@@ -1,87 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
-    public ListNode removeElements(ListNode head, int val, MyLinkedList list) {
-        ListNode dummy = new ListNode(-1, head);
-        ListNode prev = dummy;
-        ListNode curr = head;
+    public int removeElement(int[] nums, int val) {
 
-        while (curr != null) {
-            if (curr.val == val) {
-                ListNode newCurr = curr.next;
-                prev.next = newCurr;
-                curr = newCurr;
-            } else {
-                prev = curr;
-                curr = curr.next;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
             }
         }
-        list.printList();
-        return dummy.next;
+
+        System.out.println(nums);
+        return count;
 
     }
 
     public static void main(String[] args) {
-        MyLinkedList MyLinkedListA = new MyLinkedList();
-        MyLinkedListA.insertAtLast(1);
-        MyLinkedListA.insertAtLast(2);
-        MyLinkedListA.insertAtLast(6);
-        MyLinkedListA.insertAtLast(3);
-        MyLinkedListA.insertAtLast(4);
-        MyLinkedListA.insertAtLast(5);
-        MyLinkedListA.insertAtLast(6);
-        Solution m = new Solution();
-        m.removeElements(MyLinkedListA.root, 6, MyLinkedListA);
-    }
-}
+        int[] nums = { 3, 2, 2, 3 };
+        int val = 2;
+        Solution s = new Solution();
+        int returned = s.removeElement(nums, val);
+        System.out.println(returned);
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
-class MyLinkedList {
-    ListNode root;
-
-    MyLinkedList() {
-        this.root = null;
-    }
-
-    void insertAtLast(int value) {
-        ListNode newNode = new ListNode(value, null);
-
-        if (root == null) {
-            root = newNode;
-        } else {
-            ListNode currNode = root;
-            while (currNode.next != null) {
-                currNode = currNode.next;
-            }
-            currNode.next = newNode;
-        }
-    }
-
-    void printList() {
-        if (root == null) {
-            System.out.println("List is Empty");
-            return;
-        }
-        ListNode currNode = root;
-        while (currNode != null) {
-            System.out.println(currNode.val);
-            currNode = currNode.next;
-        }
-        System.out.println('\n');
     }
 }
