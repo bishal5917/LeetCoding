fe799a4f1a1e8b2575265d4cedb1e72ecf497b7e
bishal5917 <poudelb172@gmail.com>
2023-11-25 16:22:41 +0545

    Teemo Attacking

---
+++ TeemoAttacking/TeemoAttacking.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeemoAttacking {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if (timeSeries.length == 0) {
+            return 0;
+        }
+        int total = 0;
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            total += Math.min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+        total += duration;
+        System.out.println(total);
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] timeseries = { 1, 2, 3, 4, 5 };
+        int duration = 5;
+        TeemoAttacking obj = new TeemoAttacking();
+        obj.findPoisonedDuration(timeseries, duration);
+    }
+}
