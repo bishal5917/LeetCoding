aaa261d5fe6f818a260765428e07854e17627560
bishal5917 <poudelb172@gmail.com>
2023-11-30 20:12:24 +0545

    Degree of an array finally solved 🔥

--- DegreeOfArray.java
+++ DegreeOfArray.java
@@ -1,30 +1,46 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DegreeOfArray {
 
     public int findShortestSubArray(int[] nums) {
-        int degree = 1;
-        // Arrays.sort(nums);
-        int currDegree = 1;
-        int i = 0;
-        while (i < nums.length - 1) {
-            if (nums[i] != nums[i + 1]) {
-                degree = Math.max(degree, currDegree);
-                currDegree = 1;
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        HashMap<Integer, Integer> firstSeenIdxs = new HashMap<>();
+        HashMap<Integer, Integer> lastSeenIdxs = new HashMap<>();
+        AtomicInteger minSubarrayLength = new AtomicInteger(50001);
+        AtomicInteger maxDegree = new AtomicInteger(Integer.MIN_VALUE);
+        for (int i = 0; i < nums.length; i++) {
+            int item = nums[i];
+            if (counts.containsKey(item)) {
+                int previousVal = counts.get(item);
+                int newVal = previousVal + 1;
+                maxDegree.set( Math.max(maxDegree.get(), newVal));
+                counts.put(item, newVal);
+                lastSeenIdxs.put(item, i);
             } else {
-                currDegree += 1;
+                counts.put(item, 1);
+                firstSeenIdxs.put(item, i);
+                lastSeenIdxs.put(item, i);
             }
-            i += 1;
         }
-        degree = Math.max(degree, currDegree);
-        System.out.println(degree);
-        return degree;
+        System.out.println(maxDegree);
+        System.out.println(counts);
+        System.out.println(firstSeenIdxs);
+        System.out.println(lastSeenIdxs);
+        counts.forEach((key, value) -> {
+            if (value == maxDegree.get()) {
+                // differ first and last seen idx
+                int diff = lastSeenIdxs.get(key) - firstSeenIdxs.get(key) + 1;
+                minSubarrayLength.set(Math.min(minSubarrayLength.get(), diff));
+            }
+        });
+        System.out.println(minSubarrayLength.get()==50001 ? 1 : minSubarrayLength.get());
+        return minSubarrayLength.get()==50001 ? 1 : minSubarrayLength.get();
     }
 
     public static void main(String[] args) {
         DegreeOfArray obj = new DegreeOfArray();
-        int[] nums = { 1, 2, 2, 3, 1, 4, 2 };
+        int[] nums = { 1};
         obj.findShortestSubArray(nums);
-
     }
 }
