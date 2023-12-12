bd97043cb572e7787aab2bc33aa7ee5b4c645fb2
bishal5917 <poudelb172@gmail.com>
2023-12-12 20:41:32 +0545

    HeightChecker problem solved

---
+++ HeightChecker.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HeightChecker {
+
+    public int heightChecker(int[] heights) {
+
+        ArrayList<Integer> unsorted = new ArrayList<>();
+        for (int item : heights) {
+            unsorted.add(item);
+        }
+
+        int i = 0;
+        int count = 0;
+        Arrays.sort(heights);
+
+        while (i < heights.length) {
+            if (heights[i] != unsorted.get(i)) {
+                count += 1;
+            }
+            i+=1;
+        }
+
+        System.out.println(count);
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        HeightChecker obj = new HeightChecker();
+        int[] heights = { 1, 1, 4, 2, 1, 3 };
+        obj.heightChecker(heights);
+    }
+}
