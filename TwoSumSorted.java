76e8453d08e0b63710afd49f66fbd3bf47f786c0
bishal5917 <poudelb172@gmail.com>
2023-12-22 16:30:12 +0545

    Two Sums on sorted array,O(n) time complexity

---
+++ TwoSumSorted.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoSumSorted {
+
+    public int[] twoSum(int[] numbers, int target) {
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = numbers.length - 1;
+
+        while (i <= j) {
+            if (numbers[i] + numbers[j] == target) {
+                ans.add(i + 1);
+                ans.add(j + 1);
+                break;
+            } else if (numbers[i] + numbers[j] > target) {
+                j -= 1;
+            } else {
+                i += 1;
+            }
+        }
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int target = 9;
+        int[] nums = { 2, 7, 11, 15 };
+        TwoSumSorted obj = new TwoSumSorted();
+        obj.twoSum(nums, target);
+    }
+
+}
