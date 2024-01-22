8af0d50599d68a1c35c0c934a52a38f6655b1497
bishal5917 <poudelb172@gmail.com>
2024-01-22 15:04:59 +0545

    BinaryTreePathSumII solved : Recursive Backtracking

---
+++ BinaryTreePathSumII.java
@@ -0,0 +1,59 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTreePathSumII {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        return findPathsHelper(root, new ArrayList<>(), new ArrayList<>(), 0, targetSum);
+    }
+
+    public List<List<Integer>> findPathsHelper(TreeNode root, List<List<Integer>> paths, List<Integer> currPath,
+            int currSum, int target) {
+        if (root == null) {
+            return paths;
+        }
+        currSum = currSum + root.val;
+        currPath.add(root.val);
+        if (root.left == null && root.right == null) {
+            if (currSum == target) {
+                paths.add(new ArrayList<>(currPath));
+            }
+            // Remove the last element - Backtrack
+            // Decrement the current element from root - Backtrack
+            currPath.remove(currPath.size() - 1);
+            currSum=currSum-root.val;
+            return paths;
+        }
+        paths = findPathsHelper(root.left, paths, new ArrayList<>(currPath), currSum, target);
+        paths = findPathsHelper(root.right, paths, new ArrayList<>(currPath), currSum, target);
+        return paths;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        BinaryTreePathSumII obj = new BinaryTreePathSumII();
+        List<List<Integer>> paths = obj.pathSum(root, 4);
+        System.out.println(paths);
+    }
+}
