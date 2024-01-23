2d36c82f37b12d4108b99be57e2c82659fcba2ec
bishal5917 <poudelb172@gmail.com>
2024-01-23 14:58:26 +0545

    Insert in BST solved : Recursion

---
+++ InsertInBST.java
@@ -0,0 +1,66 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InsertInBST {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode newNode = new TreeNode(val);
+        if (root == null) {
+            root = newNode;
+            return root;
+        }
+        insert(root, newNode);
+        return root;
+    }
+
+    private void insert(TreeNode root, TreeNode node) {
+        if (root.left == null && root.right == null) {
+            if (node.val < root.val) {
+                root.left = node;
+                return;
+            } else {
+                root.right = node;
+                return;
+            }
+        }
+        if (root.left == null && node.val < root.val) {
+            root.left = node;
+            return;
+        }
+        if (root.right == null && node.val > root.val) {
+            root.right = node;
+            return;
+        }
+        if (node.val > root.val) {
+            insert(root.right, node);
+        } else {
+            insert(root.left, node);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        InsertInBST obj = new InsertInBST();
+        System.out.println(obj.insertIntoBST(root, 5).val);
+    }
+}
