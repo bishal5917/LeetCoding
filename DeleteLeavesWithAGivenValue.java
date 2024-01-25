0194e23270d23dc119432ac2cea6326d3937b969
bishal5917 <poudelb172@gmail.com>
2024-01-25 16:31:16 +0545

    Delete leaves with a given value solved !!!

---
+++ DeleteLeavesWithAGivenValue.java
@@ -0,0 +1,46 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class DeleteLeavesWithAGivenValue {
+
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        return removeLeavesHelper(root, target);
+    }
+
+    private TreeNode removeLeavesHelper(TreeNode root, int target) {
+        if (root == null) {
+            return root;
+        }
+        root.left = removeLeavesHelper(root.left, target);
+        root.right = removeLeavesHelper(root.right, target);
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(2);
+        root.left.left.left = new TreeNode(2);
+        DeleteLeavesWithAGivenValue obj = new DeleteLeavesWithAGivenValue();
+        obj.removeLeafNodes(root, 2);
+    }
+}
