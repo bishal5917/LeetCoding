2ddcc5a730d56f61931cd0cec629daf1303e72d9
bishal5917 <poudelb172@gmail.com>
2024-01-26 10:35:29 +0545

    kth largest sum in a binary tree solved !!!

---
+++ KthLargestSumInBinaryTree.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class KthLargestSumInBinaryTree {
+
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        List<Long> sums = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return -1;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Long currentLevelSum = 0L;
+            for (int i = 0; i < size; i++) {
+                root = queue.poll();
+                currentLevelSum += root.val;
+                if (root.left != null) {
+                    queue.offer(root.left);
+                }
+                if (root.right != null) {
+                    queue.offer(root.right);
+                }
+            }
+            sums.add(currentLevelSum);
+        }
+        if (k > sums.size()) {
+            return -1;
+        }
+        Collections.sort(sums, Collections.reverseOrder());
+        return sums.get(k - 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(8);
+        root.right = new TreeNode(9);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(1);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(7);
+        root.left.left.left = new TreeNode(4);
+        root.left.left.right = new TreeNode(6);
+        KthLargestSumInBinaryTree obj = new KthLargestSumInBinaryTree();
+        System.out.println(obj.kthLargestLevelSum(root, 2));
+    }
+}
