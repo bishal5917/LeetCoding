ee8f13b409f3d5ed113e3d0e9abd0303d1915fcb
bishal5917 <poudelb172@gmail.com>
2024-02-10 21:52:14 +0545

    Island perimeter finally solved !!!

--- IslandPerimeter/IslandPerimeter.java
+++ IslandPerimeter/IslandPerimeter.java
@@ -1,16 +1,55 @@
 package IslandPerimeter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IslandPerimeter {
 
+    // More optimal DFS solution
     public int islandPerimeter(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    return findIslandPerimeterHelper(grid, i, j, 0);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int findIslandPerimeterHelper(int[][] grid, int row, int col, int perimeter) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == 0) {
+            // means we reached the limit
+            perimeter += 1;
+            return perimeter;
+        }
+        // If we encounter already visited one,just return, dont add the perimter
+        if (grid[row][col] == -1) {
+            return perimeter;
+        }
+        // Mark traversed as visited
+        grid[row][col] = -1;
+
+        // traverse all directions
+        // GO RIGHT
+        perimeter = findIslandPerimeterHelper(grid, row + 1, col, perimeter);
+        // GO DOWN
+        perimeter = findIslandPerimeterHelper(grid, row, col + 1, perimeter);
+        // GO LEFT
+        perimeter = findIslandPerimeterHelper(grid, row, col - 1, perimeter);
+        // GO UP
+        perimeter = findIslandPerimeterHelper(grid, row - 1, col, perimeter);
+        return perimeter;
+    }
+
+    // BFS solution that dosnot work for large input (LESS OPTIMAL)
+    public int islandPerimeter2(int[][] grid) {
         int perimeter = 0;
         List<List<Integer>> queue = new ArrayList<>();
         List<List<Integer>> visited = new ArrayList<>();
-        queue.add(List.of(0, 0));
-        visited.add(List.of(0, 0));
+        queue.add(Arrays.asList(0, 0));
+        visited.add(Arrays.asList(0, 0));
         if (grid[0][0] != 0) {
             List<List<Integer>> neighbs = getNeighbors(0, 0,
                     grid);
@@ -50,16 +89,16 @@ public class IslandPerimeter {
         List<List<Integer>> neighbors = new ArrayList<>();
 
         if ((col + 1) <= graph[0].length - 1) {
-            neighbors.add(List.of(row, col + 1));
+            neighbors.add(Arrays.asList(row, col + 1));
         }
         if ((col - 1) >= 0) {
-            neighbors.add(List.of(row, col - 1));
+            neighbors.add(Arrays.asList(row, col - 1));
         }
         if ((row - 1) >= 0) {
-            neighbors.add(List.of(row - 1, col));
+            neighbors.add(Arrays.asList(row - 1, col));
         }
         if ((row + 1) <= graph.length - 1) {
-            neighbors.add(List.of(row + 1, col));
+            neighbors.add(Arrays.asList(row + 1, col));
         }
         return neighbors;
     }
@@ -69,6 +108,7 @@ public class IslandPerimeter {
         // int[][] grid = { { 0, 0, 1 } };
         IslandPerimeter ip = new IslandPerimeter();
         System.out.println(ip.islandPerimeter(grid));
+        System.out.println(ip.islandPerimeter2(grid));
     }
 
 }
