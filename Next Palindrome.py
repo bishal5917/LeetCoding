7c55d9d73a8815ad13ac54b27cf50901418a3e0b
bishal5917 <poudelb172@gmail.com>
2024-02-23 11:10:22 +0545

    refactor and separation

--- Next Palindrome.py
+++
@@ -1,9 +0,0 @@
-
-def pal_check():
-    n=int(input("Enter the number you want the next palindrome of"))
-
-    while (str(n) != str(n)[::-1]):
-        n=n+1
-    print(n)
-
-pal_check()
--- findKthLargest.py
+++
@@ -1,9 +0,0 @@
-
-def findKLargest(K):
-    array = [1, 2, 3, 4, 5]
-    array.sort()
-
-    return array[::-1][K-1]
-
-
-print(findKLargest(3))
