d595141b1a9318b0e21eeba5cc76f33d7ba385fa
bishal5917 <poudelb172@gmail.com>
2026-05-10 10:39:09 +0545

    Levenshtein Distance (DP + BruteForce)

--- Python/LevenshteinDistance.py
+++ Python/LevenshteinDistance.py
@@ -1,16 +1,62 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
 
-def LD():
-    str1 = "passed"
-    str2 = "past"
-    arr = [[x for x in range(len(str2)+1)] for y in range(len(str1)+1)]
-    for i in range(1, len(str1)+1):
-        arr[i][0] = arr[i-1][0]+1
-    for i in range(1, len(str1)+1):  # i=row
-        for j in range(1, len(str2)+1):  # j=column
-            if str1[i-1] == str2[j-1]:
-                arr[i][j] = arr[i-1][j-1]
-            else:
-                arr[i][j] = 1+min(arr[i-1][j-1], arr[i-1][j], arr[i][j-1])
-    return arr[-1][-1]
+        word1len = len(word1)
+        word2len = len(word2)
+    
+        # Dynamic programming - Bottom up approach
+        # Lets make a dp table
+        dp = [[0] * (word2len + 1) for _ in range(word1len + 1)]
+        # Assign with values on the last columns and row
+        for i in range(word1len):
+            dp[i][word2len] = word1len - i
+        for i in range(word2len+1):
+            dp[word1len][i] = word2len - i
 
-print(LD())
+        # Bottom Up Iteration
+
+        for i in reversed(range(word1len)):
+            for j in reversed(range(word2len)):
+                # Now check and assign the required distance in a bottom up manner
+                if word1[i] == word2[j]:
+                    # just assign with that of lower diagonal
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    # calculate the minimum in three directions and assign with that by incrementing 1
+                    dp[i][j] = min(dp[i+1][j+1], dp[i][j+1], dp[i+1][j]) + 1
+
+        print(dp)
+        return dp[0][0]
+        
+    def minDistanceBruteforce(self, word1, word2):
+        # for each position (we either insert, delete, or update)
+        # And calculate the minimum distance
+        return self.minDistanceBruteforceRecursiveHelper(word1, word2, 0, 0)
+                    
+    def minDistanceBruteforceRecursiveHelper(self, word1, word2, i, j):
+        # Base cases
+        # If word1 exhausted
+        if i == len(word1):
+            return len(word2) - j
+
+        # If word2 exhausted
+        if j == len(word2):
+            return len(word1) - i
+        
+        # Characters match
+        if word1[i] == word2[j]:
+            return self.minDistanceBruteforceRecursiveHelper(word1, word2, i+1, j+1)
+        
+        # Try all 3 operations
+        insert_op = 1 + self.minDistanceBruteforceRecursiveHelper(word1,word2,i,j+1)
+        delete_op = 1 + self.minDistanceBruteforceRecursiveHelper(word1,word2,i+1,j)
+        replace_op = 1 + self.minDistanceBruteforceRecursiveHelper(word1,word2,i+1,j+1)
+
+        return min(insert_op,delete_op,replace_op)
+
+if __name__ == "__main__":
+    sol = Solution()
+    word1 = "horse"
+    word2 = "ros"
+    print(sol.minDistance(word1, word2))
+    print(sol.minDistanceBruteforce(word1, word2))
